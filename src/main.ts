cc0580413166c770dedd093c3343be9b6f38a592
Fabrina Nunes <fabrina@MacBook-Pro-de-Fabrina-2.local>
2022-04-09 21:42:08 -0300

    chore: API doc genarated with swagger

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,20 @@
 import { NestFactory } from '@nestjs/core';
+import { ValidationPipe } from '@nestjs/common';
 import { AppModule } from './app.module';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.useGlobalPipes(new ValidationPipe({ errorHttpStatusCode: 422 }));
+
+  const config = new DocumentBuilder()
+    .setTitle('Imersão FullCycle - API Rest')
+    .setDescription('API Rest utilizando Nest.JS')
+    .build();
+
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, document);
+
   await app.listen(3000);
 }
 bootstrap();
